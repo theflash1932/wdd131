c3ed86e33efe896dc5fd1206981891c3a6e7e71c
theflash1932 <greg@gregnielson.com>
2025-07-24 13:04:26 -0600

    modified:   scripts/temples.js 	modified:   styles/temples.css

--- scripts/temples.js
+++ scripts/temples.js
@@ -7,3 +7,11 @@ hambutton.addEventListener('click', () => {
 	mainnav.classList.toggle('show');
 	hambutton.classList.toggle('show');
 });
+
+cdate = document.querySelector("#currentyear");
+nowYear = new Date().getFullYear();
+cdate.innerHTML = nowYear;
+
+lastm = document.querySelector("#lastmodified");
+mod = document.lastModified
+lastm.innerHTML = document.lastModified
