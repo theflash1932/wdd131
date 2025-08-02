f45c8b7f3ba5e822cff85d59c25b87f8c476d9d7
theflash1932 <greg@gregnielson.com>
2025-08-02 16:45:03 -0600

    new file:   images/conditions.svg 	new file:   images/italy-narrow.jpg 	new file:   images/italy-narrow.webp 	new file:   images/italy-widejpg.jpg 	new file:   images/italy-widejpg.webp 	new file:   place.html 	new file:   scripts/place.js 	new file:   styles/place.css 	modified:   temples.html

---
+++ scripts/place.js
@@ -0,0 +1,31 @@
+cdate = document.querySelector("#currentyear");
+nowYear = new Date().getFullYear();
+cdate.innerHTML = nowYear;
+
+lastm = document.querySelector("#lastmodified");
+mod = document.lastModified
+lastm.innerHTML = "Last modified: " + document.lastModified
+
+currentTemp = 8.2
+currentWind = 5.2
+wc = document.querySelector("#wc");
+cond = document.querySelector("#cond")
+wind = document.querySelector("#wind")
+temp = document.querySelector("#temp")
+windchill = currentTemp
+if(currentTemp < 10.0 && currentWind > 4.8) {
+    windchill = calculateWindChill().toFixed(1);
+}
+else
+    windchill = "N/A"
+
+function calculateWindChill() {
+    return windchill = 13.12 + (0.6215 * currentTemp) - (11.37 * (currentWind ** 0.16)) + (0.3965 * currentTemp * (currentWind ** 0.16));
+}
+now.innerHTML = "<img src='images/conditions.svg' alt='now'>"
+cond.innerHTML = "Mostly Sunny"
+wind.innerHTML = currentWind + " km/s"
+temp.innerHTML = currentTemp + " C"
+wc.innerHTML = windchill + " C"
+
+//only call calcwindchill if temp < 10 C and wind > 4.8 km/h, else display N/A
